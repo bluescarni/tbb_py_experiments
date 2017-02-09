6189a03889aac8e4dcf228e683fc12b58186e25f
Francesco Biscani <bluescarni@gmail.com>
2017-02-09 12:30:19 +0100

    More work.

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
+from time import sleep
+
 def func(n):
-    import time, random
-    time.sleep(random.uniform(0,1))
+    import random
+    sleep(random.uniform(0,1))
     print("Processing item {}".format(n))
 
 def func2():
     import time, random
-    time.sleep(random.uniform(0,1))
+    sleep(random.uniform(0,1))
     print("Done!")
 
+print("pybind11, tbb::task_group test")
+print("==============================")
+sleep(1)
 import pb_tg
 
 tg = pb_tg.task_group()
@@ -18,6 +23,9 @@ print("Waiting now!!!")
 
 tg.wait()
 
+print("boost.python, tbb::task_group test")
+print("==================================")
+sleep(1)
 import bp_tg
 
 tg = bp_tg.task_group()
@@ -28,10 +36,30 @@ print("Waiting now!!!")
 
 tg.wait()
 
+print("pybind11, c++11 parallel_for test")
+print("=================================")
+sleep(1)
+import pb_parfor
+
+pb_parfor.parallel_for(0,100,func)
+
+print("boost.python, c++11 parallel_for test")
+print("=====================================")
+sleep(1)
 import bp_parfor
 
 bp_parfor.parallel_for(0,100,func)
 
-import pb_parfor
+print("pybind11, TBB parallel_for test")
+print("===============================")
+sleep(1)
+import pb_tbb_parfor
 
 pb_parfor.parallel_for(0,100,func)
+
+print("boost.python, TBB parallel_for test")
+print("===================================")
+sleep(1)
+import bp_tbb_parfor
+
+bp_parfor.parallel_for(0,100,func)
